09dea4d9de0abfc55ed851c8f33ff6e7c7adf8ed
Eric Bomblatus <ebomblatus@gmail.com>
2017-01-06 15:46:11 -0800

    build JS files but not returning results

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,5 +1,19 @@
 var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
-  
-}
+ var currentDoctorObject = new Doctor();
+ $('.showDoctors').hide();
+ $('#results').hide();
+ $('#search').click(function() {
+   event.preventDefault();
+   $('.showDoctors').empty();
+
+   var symptom = $('#symptom').val();
+   $('#symptom').val("");
+
+   currentDoctorObject.getDoctors(symptom);
+
+   $('.showDoctors').show();
+   $('#results').show();
+ });
+});
--- js/doctor.js
+++ js/doctor.js
@@ -1,11 +1,17 @@
-var apiKey = require('./../.env').apiKey;
-
-exports.getDoctors = function(medicalIssue) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
-   .then(function(result) {
-      console.log(result);
-    })
-   .fail(function(error){
-      console.log("fail");
-    });
+Doctor = function(medicalIssue) {
+  this.medicalIssue = medicalIssue;
 };
+
+Doctor.prototype.getDoctors = function(medicalIssue) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue +'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey).then(function(response) {
+    $('.showDoctors').text(response.doctors.forEach(function(doctor) {
+      $('.showDoctors').append("<li>" + doctor.profile + " " + "</li>");
+      console.log(response);
+    }));
+  })
+    .fail(function(error){
+    $('.$showDoctors').text(error.responseJSON.message);
+  });
+}; 
+
+exports.doctorModule = Doctor;
