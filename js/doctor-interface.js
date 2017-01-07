11a07d31857e6de31b54ea19dc976f9d9faf7e6c
Eric Bomblatus <ebomblatus@gmail.com>
2017-01-06 16:54:22 -0800

    final commit for the day

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -2,3 +2,6 @@ var doctorData = require('./../js/doctor.js').doctorModule;
 $(document).ready(function() {
  $('#doctor-search').submit(function(event) {
    event.preventDefault();
+ });
+});
+//LOLLOLLOLLOLLOLLOLLOLLOLLOL
