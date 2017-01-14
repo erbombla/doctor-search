44668373de3c1fcf8d80a6e544998c9bea78aaae
Eric <ebomblatus@gmail.com>
2017-01-14 13:24:22 -0800

    refactor code

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -2,6 +2,10 @@ var doctorData = require('./../js/doctor.js').doctorModule;
 $(document).ready(function() {
  $('#doctor-search').submit(function(event) {
    event.preventDefault();
+   var input = $("symptom").val();
+   var results = doctorData.getDoctors(input);
+   results.forEach(function(data) {
+     $(".showDoctors").append(data)
+   };
  });
 });
-//LOLLOLLOLLOLLOLLOLLOLLOLLOL
--- js/doctor.js
+++ js/doctor.js
@@ -7,11 +7,12 @@ Doctor.prototype.getDoctors = function(medicalIssue) {
   var doctors = [];
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue +'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
     .then(function(result) {
-      console.log(result);
+      doctors = result
     })
     .fail(function(error){
       console.log("error.responseJSON.message");
     });
+    return doctors
 };
 
 exports.doctorModule = Doctor;
